f7d61af111c91a64c386f1100a0e72dc15aefc63
1damsemib <1damsemib@PC235004.informatica.edu>
2025-06-03 20:50:26 +0100

    segundo commit - prueba

--- src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -11,6 +11,6 @@ package com.mycompany.mavenproject1;
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-        System.out.println("AWA");
+        System.out.println("oye siri no mas awa");
     }
 }
